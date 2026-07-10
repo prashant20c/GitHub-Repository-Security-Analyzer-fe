29abb1cef4915fa45a810490bf78052e900ac13f
Prashant Poudel <pirate20b@gmail.com>
2026-07-10 12:25:07 +1000

    chore: update Vite API proxy

--- vite.config.js
+++ vite.config.js
@@ -8,8 +8,9 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
-        changeOrigin: true
+        target: 'http://nginx:80',
+        changeOrigin: true,
+        secure: false
       }
     }
   }
